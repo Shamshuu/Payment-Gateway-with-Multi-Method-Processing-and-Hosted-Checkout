6b597256559e6631c05aa0c54018d0708125a8bc
Shamshuu <shamshu2004@gmail.com>
2026-01-07 23:35:05 +0530

    Update TestController

--- backend/src/main/java/com/gateway/controllers/TestController.java
+++ backend/src/main/java/com/gateway/controllers/TestController.java
@@ -1,6 +1,5 @@
 package com.gateway.controllers;
 
-import com.gateway.models.Merchant;
 import com.gateway.repositories.MerchantRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
